14ddfddc57182ca6c633762cfe8a43edf1cabf75
Jesse Jensen <jjensen@ebsco.com>
2015-06-30 18:07:12 +0000

    Trying to package

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ except ImportError:
 
 setup(
   name='ebscopy',
-  version='0.0.1dev',
+  version='0.0.2.dev0',
   author='Jesse Jensen',
+  author_email='jjensen@ebsco.com',
+  url='https://github.com/jessejensen/ebscopy',
   packages=['ebscopy','ebscopy.test'],
+  include_package_data=True,
   description='Official Python wrapper for EBSCO Discover Service (EDS) API',
-  long_description=open('README.md').read(),
+  long_description=open('README.txt').read(),
   install_requires=[
           'requests',
           'datetime',
