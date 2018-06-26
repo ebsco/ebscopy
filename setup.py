8a132a7ecd056069523f502fc76af2a811381028
Jesse Jensen (EBSCO) <jjensen@ebsco.com>
2018-06-26 23:47:49 +0000

    Release v0.1.18

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except:
 # Regular setup data
 setup(
 	name					= 'ebscopy',
-	version					= '0.1.17',
+	version					= '0.1.18',
 	author					= 'Jesse Jensen',
 	author_email			= 'jjensen@ebsco.com',
 	url						= 'https://github.com/jessejensen/ebscopy',
